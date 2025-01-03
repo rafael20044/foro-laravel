88162848a9c8f7247ca3a86529727e490ca98347
Rafael Barragán Acevedo <70608879+rafael20044@users.noreply.github.com>
2025-01-03 16:20:16 -0500

    layout configurado

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
+    <link rel="stylesheet" href="/">
 </head>
 <body>
     <header>
@@ -13,34 +14,75 @@
             <a href="{{ route('about') }}">about</a>
         </nav>
         <input type="text" name="" id="" placeholder="Buscar">
-        <a href="">repository on GitHub</a>
+        <a href="https://github.com/rafael20044/foro-laravel" target="_black">repository on GitHub</a>
         <div class="log">
             <a href="" class="login">Log in</a>
             <a href="" class="signUp">Sign up</a>            
         </div>
     </header>
-    <div class="nav2">
-        <a href=""></a>
+    <div class="sidebar">
+        <p>header 2</p>
     </div>
     <main>
         {{ $slot }}
     </main>
 </body>
 </html>
-
 <style>
     body{
         padding: 0;
         margin: 0;
         box-sizing: border-box
     }
+    body{
+        display: grid;
+        grid-template-areas: 
+            "header header header"
+            "sidebar main main"
+            "sidebar main main";
+        grid-template-columns: 1fr 3fr 3fr;
+    }
     header{
+        grid-area: header;
         display: flex;
         align-items: center;
         align-content: center;
-        gap: 10rem
+        justify-content: center;
+        gap: 6rem;
+        border: 2px solid black;
+        padding: 1rem;
+    }
+    header a{
+        text-decoration: none;
     }
     header input{
         width: 25rem;
+    }
+    header .log{
+        display: flex;
+        gap: 1rem;
+    }
+    header .log a{
+        padding: 2px 0.5rem;
+    }
+    header .log .login{
+        border: 2px solid blue;
+        border-radius: 0.5rem;
+    }
+    header .log .signUp{
+        border: 2px solid blue;
+        border-radius: 0.5rem;
+        background-color: blue;
+        color: white;
+    }
+    .sidebar{
+        grid-area: sidebar;
+        border: 2px solid black;
+        padding: 1rem;
+    }
+    main{
+        grid-area: main;
+        border: 2px solid black;
+        padding: 1rem;
     }
 </style>
